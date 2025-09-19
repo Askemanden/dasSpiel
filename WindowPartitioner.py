378283348f8edef18887975b384c73ea1922008e
Jacob Fischer Bak <jacobfbak@gmail.com>
2025-09-19 15:03:28 +0200

    Jeg har lavet simple UI-menuer

--- WindowPartitioner.py
+++ WindowPartitioner.py
@@ -2,8 +2,12 @@ import pygame
 from vectors import Vector2f
 import enum
 
+class alignment(enum.Enum):
+    horizontal = 0
+    vertical = 1
+
 class screen_box:
-    def __init__(self, x, y, width, height):
+    def __init__(self, x, y, width, height, button_alignment = alignment.horizontal):
         self.screen_position_x = x - width * 0.5
         self.screen_position_y = y - height * 0.5
         self.rect = pygame.Rect(self.screen_position_x, self.screen_position_y, width, height)
@@ -14,17 +18,15 @@ class screen_box:
         self.color = (120,105,105)
        
         self.containers = []
+        self.button_alignment = button_alignment
 
     def add_container(self, container):                                 # Tilføjer en container til listen af beholdere.
-        if container.on_click == intet and container.type == button:    # Hvis containeren ikke har en funktion, så kan den ikke tilføjes.
-            return False
-        
         self.containers.append(container)                               # En container kan være en knap eller en holder til en item.
         self.update_container_rects()
         return True
     
-    def update(self):
-        mouse_pos = pygame.mouse.get_pos()
+    def update(self):                                                   # Opdaterer tilstanden af screen_boxen og dens containere.
+        mouse_pos = pygame.mouse.get_pos()                              # update kalder ikke den matematiktunge updtate_container_rects, da den kun behøver at blive kaldt når en container tilføjes eller fjernes, eller kassen flyttes.
 
         for container in self.containers:
             if container.rect.collidepoint(mouse_pos):
@@ -37,14 +39,26 @@ class screen_box:
         for container in self.containers:
             length = len(self.containers)
 
-            if container.container_type == container_type.button:
-                container.rect.width = (self.rect.width - self.border_width * 2) / (length - self.border_width * 2)
-                container.rect.height = 30  # Arbitrær værdi for knap højde.
+            if self.button_alignment == alignment.horizontal:
+               if container.container_type == container_type.button:
                 index = self.containers.index(container)
-                container.rect.topleft = (self.rect.top + (self.rect.height - self.border_width * 2),self.rect.left + self.border_width + index * container.rect.width)
-            
-                
 
+                container.rect.width = (self.rect.width - (length + 3) * self.border_width)/ length
+                container.rect.height = 40  # Arbitrær værdi for knap højde.
+                
+                container.rect.x = self.rect.x + self.border_width * 2 + index * (container.rect.width + self.border_width)
+                container.rect.y = self.rect.y + self.rect.height - self.border_width * 2 - container.rect.height
+
+            elif self.button_alignment == alignment.vertical:
+                if container.container_type == container_type.button:
+                    index = self.containers.index(container)
+
+                    container.rect.width = self.rect.width * 0.5 - self.border_width * 2
+                    container.rect.height = 40  # Arbitrær værdi for knap højde.
+                    
+                    container.rect.x = self.rect.x + self.border_width
+                    container.rect.y = self.rect.y + self.rect.height - self.border_width * 2 - (length - index) * (container.rect.height + self.border_width)
+            
     def move(self, x, y):
         self.screen_position_x = x - self.rect.width * 0.5
         self.screen_position_y = y - self.rect.height * 0.5
@@ -65,14 +79,40 @@ class screen_box:
         for container in self.containers:
             container.draw(screen)
 
+
+class popup_menu_types(enum.Enum):
+    OK_CANCEL = 0
+    YES_NO = 1
+    QUIT = 2
+
+class popup_menu(screen_box):
+    def __init__(self, x, y, width, height, type):
+        super().__init__(x, y, width, height, button_alignment = alignment.horizontal)
+        self.type = type
+        if self.type == popup_menu_types.OK_CANCEL:
+            pass
+        elif self.type == popup_menu_types.YES_NO:
+            pass
+        elif self.type == popup_menu_types.QUIT:
+            
+            return_button = container(container_type.button, intet)
+            return_button.color = (34, 139, 34)  # Grøn farve for return knappen.
+            self.add_container(return_button)
+
+            quit_button = container(container_type.button, quit)
+            quit_button.color = (220, 20, 60)  # Rød farve for quit knappen.
+            self.add_container(quit_button)
+            
+
+
+
+
 def intet():
     pass
 
 def quit():
     pygame.quit()
-    exit()
-
-
+    quit()
 
 class container_type(enum.Enum):
     item = 0
@@ -99,6 +139,8 @@ class container:
         else:
             pygame.draw.rect(screen, self.color, self.rect)
 
+
+
 # Testkode
 if __name__ == ("__main__"):
     pygame.init()
@@ -107,11 +149,20 @@ if __name__ == ("__main__"):
     running = True
     screen_box = screen_box(200, 200, 300, 300)
 
-    button = container(container_type.button, pygame.quit)
-    screen_box.add_container(button)
+    #button1 = container(container_type.button, quit)
+    #screen_box.add_container(button1)
+    #button2 = container(container_type.button, quit)
+    #screen_box.add_container(button2)
+
+    #text_box = text_box(400, 250, 300, 200, "Dette er en test")
+
+    popup_menu = popup_menu(400, 250, 300, 200, popup_menu_types.QUIT)
 
 
     while running:
+        popup_menu.update()
+        
+
         for event in pygame.event.get():                    # Håndterer begivenheder som lukning af vindue og museklik.
             
             if event.type == pygame.QUIT:
@@ -119,16 +170,17 @@ if __name__ == ("__main__"):
             
             elif event.type == pygame.MOUSEBUTTONDOWN:      # Håndterer museklik.
                 mouse_pos = event.pos
-                screen_box.update()
-                for container in screen_box.containers:
-                    if container.rect.collidepoint(mouse_pos):
+                
+                for container in popup_menu.containers:
 
+                    if container.rect.collidepoint(mouse_pos):
                         container.on_click()
                     
             
             #elif
 
 
-        screen_box.draw(screen)
+        popup_menu.draw(screen)
         pygame.display.flip()
     pygame.quit()
+    quit()
